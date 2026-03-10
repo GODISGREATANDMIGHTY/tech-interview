fbe1b09149043669c497c4ea83118d779b1d26ad
hechengzhu <hechengzhu@hechengzhus-MacBook-Air.local>
2026-03-10 13:01:10 +0800

    add python

--- python/singleton.py
+++ python/singleton.py
@@ -1,13 +1,15 @@
-
+import threading
 
 
 
 
 class SingletonMeta(type):
     _instance = {}
+    _lock = threading.Lock()
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instance:
-            cls._instance[cls] = super().__call__(*args, **kwargs)
+        with cls._lock:
+            if cls not in cls._instance:
+                cls._instance[cls] = super().__call__(*args, **kwargs)
         return cls._instance[cls]
 
 class Singleton(metaclass=SingletonMeta):
